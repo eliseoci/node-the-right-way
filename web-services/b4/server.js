701374e14a8fe92d5b74411c8aacdc4620edf9f0
Eliseo Cohen Imach <eliseoimach@gmail.com>
2018-06-08 15:19:25 -0300

    Search for books implemented

--- web-services/b4/server.js
+++ web-services/b4/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const nconf = require("nconf");
 const pkg = require("./package.json");
+const search = require("./lib/search.js");
 
 const app = express();
 
@@ -15,5 +16,7 @@ app.use(morgan("dev"));
 app.get("/api/version", (req, res) => {
   res.status(200).send(pkg.version);
 });
+
+search(app, nconf.get("es"));
 
 app.listen(nconf.get("port"), () => console.log("Ready!"));
